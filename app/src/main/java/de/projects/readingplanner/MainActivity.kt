9a3e83d7d90ccdd2e51c95b27661802c3ff29f9a
Andrei Precup <andreiprecup69@gmail.com>
2019-04-27 16:07:36 +0300

    Floating button with menu and animation.

--- app/src/main/java/de/projects/readingplanner/MainActivity.kt
+++ app/src/main/java/de/projects/readingplanner/MainActivity.kt
@@ -1,12 +1,36 @@
 package de.projects.readingplanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fab.setOnClickListener {
+            if (menu.isVisible) {
+                menu.animate().alpha(0f)
+                    .setDuration(1000)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            menu.isVisible = false
+                        }
+                    })
+            } else {
+                menu.alpha = 0f
+                menu.animate().alpha(1f)
+                    .setDuration(1000)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationStart(animation: Animator) {
+                            menu.isVisible = true
+                        }
+                    })
+            }
+        }
+    }
 }
