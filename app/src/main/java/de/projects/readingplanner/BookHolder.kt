bd5dfdf37b668293d88674d2681a812acaa837f5
Andrei Precup <andreiprecup69@gmail.com>
2019-07-13 22:50:45 +0300

    Removed image for books in list.

--- app/src/main/java/de/projects/readingplanner/BookHolder.kt
+++ app/src/main/java/de/projects/readingplanner/BookHolder.kt
@@ -2,13 +2,11 @@ package de.projects.readingplanner
 
 import android.content.Intent
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class BookHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 	val textView: TextView = itemView.findViewById(R.id.my_text_view)
-	val imageView: ImageView = itemView.findViewById(R.id.text_image)
 
 	init {
 		itemView.setOnClickListener {
